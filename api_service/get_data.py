623a47bf2b6c2962314d4ad70fa2550cd55de407
alien <alien1900@bk.ru>
2023-11-07 15:33:41 +0700

    Sensor_id parameter was implemented

--- api_service/get_data.py
+++ api_service/get_data.py
@@ -1,6 +1,6 @@
 from .measurments import measurments_from_rows
 from .retro_measurment import retro_measurments_from_rows
-from flask import (Blueprint, json, request)
+from flask import (Blueprint, json, Request, request)
 from .db import get_db
 from pythonping import ping
 
@@ -8,9 +8,21 @@ allowed_periods = ['m1', 'm5', 'm15', 'm30', 'h1', 'h4', 'd1']
 
 bp = Blueprint('get_data', __name__, url_prefix='/get_data')
 
+def get_sensor_id(rq:Request):
+    sensor_id = request.args.get('sensor_id')
+    if not sensor_id:
+        return 0
+    return int(sensor_id)
+
 @bp.route('/retro/', methods=['GET'])
 def measurings():
     try:
+        sensor_id = get_sensor_id(request)
+        if sensor_id == 0:
+           error = 'sensor_id parameter error'
+           print(error)
+           return error, 500
+        
         db = get_db()
         period = request.args.get('period', 'm1')
         sql = get_measurings_sql(period)
@@ -23,16 +35,17 @@ def measurings():
         print(error)
         return str(error), 500
 
-def get_measurings_sql(period:str):
+def get_measurings_sql(period:str, sensor_id:int):
     period = period.lower()
     
     if not period in allowed_periods:
         period = 'm1'
     
     if(period == 'm1'):
-        return '''SELECT timestamp,temperature,humidity,sensor_id 
+        return f'''SELECT timestamp,temperature,humidity,sensor_id 
                   FROM measurings 
-                  ORDER BY timestamp DESC 
+                  WHERE sensor_id = {sensor_id}
+                  ORDER BY timestamp DESC
                   LIMIT 100'''
 
     
@@ -41,6 +54,7 @@ def get_measurings_sql(period:str):
                      avg(humidity) as humidity,
                      sensor_id
                FROM measurings 
+               WHERE sensor_id = {sensor_id}
                GROUP BY {period}
                ORDER BY timestamp DESC
                LIMIT 100'''
@@ -48,8 +62,19 @@ def get_measurings_sql(period:str):
 @bp.route('/last_timestamp/', methods=['GET'])
 def last_timestamp():
     try:
+        sensor_id = get_sensor_id(request)
+        if sensor_id == 0:
+           error = 'sensor_id parameter error'
+           print(error)
+           return error, 500
+        
         db = get_db()
-        cursor = db.execute("SELECT timestamp FROM measurings ORDER BY timestamp DESC LIMIT 1")
+        cursor = db.execute(f'''SELECT timestamp 
+                               FROM measurings 
+                               WHERE sensor_id = {sensor_id}
+                               ORDER BY timestamp 
+                               DESC LIMIT 1''')
+        
         rows = cursor.fetchall()
         last_timestamp = 0 if len(rows) == 0 else rows[0]['timestamp'] 
         return json.dumps({"last_timestamp" : last_timestamp}), 200
@@ -62,8 +87,21 @@ def last_timestamp():
 @bp.route('/last/', methods=['GET'])
 def last():
     try:
+        sensor_id = get_sensor_id(request)
+        if sensor_id == 0:
+           error = 'sensor_id parameter error'
+           print(error)
+           return error, 500
+        
         db = get_db()
-        cursor = db.execute("SELECT timestamp, temperature, humidity, DATETIME(timestamp, 'unixepoch', 'localtime') as datetime FROM measurings ORDER BY timestamp DESC LIMIT 1")
+        cursor = db.execute(f'''SELECT timestamp, 
+                                      temperature, 
+                                      humidity, 
+                                      DATETIME(timestamp, 'unixepoch', 'localtime') as datetime 
+                                FROM measurings 
+                                WHERE sensor_id = {sensor_id}
+                                ORDER BY timestamp DESC 
+                                LIMIT 1''')
         rows = cursor.fetchall()
         if len(rows) == 0:
             return 'There is no data', 500    
